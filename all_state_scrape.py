a1c052768aeed0415b09b4734c2219e182045998
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2023-07-21 13:59:53 -0500

    Selenium congig to open window

---
+++ all_state_scrape.py
@@ -0,0 +1,34 @@
+import time
+import tabula
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+DOWNLOAD_DICTIONARY = "/Users/mayrapena/PycharmProjects/low_income_effects_on_music/all_state_data"
+URL = "https://www.uiltexas.org/music/archives"
+YEARS = ["2017", "2018", "2019", "2021", "2022", "2023"]
+
+
+def set_up_driver():
+    # Set up the Chrome WebDriver
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {
+        "detach": True,
+        "download.default_directory": DOWNLOAD_DICTIONARY,
+        "download.prompt_for_download": False,  # To auto download the file
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
+    })
+
+    configured_driver = webdriver.Chrome(options=options)
+    return configured_driver
+
+
+def download_outstanding_performer():
+    driver = set_up_driver()
+    driver.get(URL)
+      
+
+
+download_outstanding_performer()
