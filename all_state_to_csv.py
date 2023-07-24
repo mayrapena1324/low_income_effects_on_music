dc951d3401dbe2b44c0cc8a6cc53fa7998cd69d1
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2023-07-24 15:38:09 -0500

    Added pdf to csv functionality that works with 2017 and older pdfs.
    Pdf plumber will handle older pdfs and camelot will handle newer pdfs.

--- all_state_to_csv.py
+++ all_state_to_csv.py
@@ -1,30 +1,42 @@
 import os
 import camelot
 import pandas as pd
+import pdfplumber
 
-# Directory path containing all the PDF files
+output_directory = "all_state_data/all_state_csvs"
 pdf_directory = "all_state_data/all_state_pdfs/"
-
-# List all the files in the directory
 pdf_files = os.listdir(pdf_directory)
+
 
 # Loop through each PDF file and process it
 for pdf_file in pdf_files:
-    # Generate the full file path
     full_pdf_path = os.path.join(pdf_directory, pdf_file)
+    year = pdf_file[0:4]
+    # Use pdf plumber for files 2017 or older
+    if int(year) > 2017:
+        tables = camelot.read_pdf(full_pdf_path, flavor='stream', pages='all')
 
-    # Load the PDF and extract tables
-    tables = camelot.read_pdf(full_pdf_path, flavor='stream', pages='all')
+        # Concatenate multiple tables from different pages
+        df = pd.concat([table.df for table in tables])
 
-    # Concatenate multiple tables from different pages
-    df = pd.concat([table.df for table in tables])
+    else:
+        with pdfplumber.open(full_pdf_path) as pdf:
+            pages = pdf.pages
 
-    # Generate the output CSV file name
-    output_file = os.path.join("all_state_data/all_state_csvs/", os.path.splitext(pdf_file)[0] + ".csv")
+            # Extract table data from each page
+            tables = []
+            for page in pages:
+                table = page.extract_table()
+                tables.extend(table)
 
-    # Save the extracted data as a CSV file
-    df.to_csv(output_file, index=False)
-    print(f"CSV file '{output_file}' has been saved in your project directory.")
+        # Convert to DataFrame
+        df = pd.DataFrame(tables)
+
+    df.to_csv(f"{output_directory}/{year}_outstanding_performers.csv", index=False)
+    print(f"CSV {year} outstanding performers file has been saved in your project directory.")
+
+
+
 
 
 
